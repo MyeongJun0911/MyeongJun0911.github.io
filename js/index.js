a106a99c3ac9425a2defd9ab7311658c2ba7125c
MyeongJun0911 <kmyeongjun124@gmail.com>
2023-11-30 17:48:38 +0900

    11-30 작업물

--- js/index.js
+++ js/index.js
@@ -1,9 +1,29 @@
+//장소 전환 함수
 function enterance(){
-    
+    window.location.href="index_in.html";
 }
 
-function sky_object1(){
-    alert('헬기지나가유');
+function leftDoor(){
+    window.location.href="index_left.html";
 }
 
+function rightDoor(){
+    window.location.href="index_right.html";
+}
+
+function showNews1(){
+    window.location.href="https://n.news.naver.com/mnews/article/584/0000025122?sid=105";
+}
+
+function showNews2(){
+    window.location.href="https://n.news.naver.com/mnews/article/087/0001010466?sid=104";
+}
+
+function showNews3(){
+    window.location.href="https://n.news.naver.com/mnews/article/009/0005223267?sid=105";
+}
+
+function showNews4(){
+    window.location.href="https://n.news.naver.com/mnews/article/014/0005108506?sid=105";
+}
 
